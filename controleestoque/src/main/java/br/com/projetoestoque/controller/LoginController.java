4db70db3d81a782ebddbad15d565307786cef05d
Lucas <lscaldeira@outlook.com>
2025-05-27 19:17:28 -0300

    Exibição de mensagem de senha ou usuario incorretos

--- controleestoque/src/main/java/br/com/projetoestoque/controller/LoginController.java
+++ controleestoque/src/main/java/br/com/projetoestoque/controller/LoginController.java
@@ -2,7 +2,7 @@ package br.com.projetoestoque.controller;
 
 import br.com.projetoestoque.dao.UsuarioDAO;
 import br.com.projetoestoque.main.App;
-import br.com.projetoestoque.util.Sessao; // Importe a classe Sessao
+import br.com.projetoestoque.util.Sessao;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -18,7 +18,7 @@ public class LoginController {
     private PasswordField passwordField;
 
     @FXML
-    private Label errorLabel;
+    private Label messageLabel; // Alinhe o fx:id no FXML para "messageLabel"
 
     private UsuarioDAO usuarioDAO = new UsuarioDAO();
 
@@ -27,14 +27,13 @@ public class LoginController {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
-        System.out.println("Tentativa de login com usuário: " + username);
+        messageLabel.setText(""); // Limpa mensagem anterior
 
         Integer usuarioId = usuarioDAO.autenticar(username, password);
         String nivel = usuarioDAO.obterNivelAcesso(username);
 
         if (usuarioId != null && nivel != null) {
-            System.out.println("Login bem-sucedido para o usuário: " + username + " | ID: " + usuarioId + " | Nível: " + nivel);
-            Sessao.setUsuarioId(usuarioId); // Salva o ID na sessão
+            Sessao.setUsuarioId(usuarioId);
 
             switch (nivel.trim().toLowerCase()) {
                 case "admin":
@@ -50,12 +49,10 @@ public class LoginController {
                     App.setRoot("view/visualizador");
                     break;
                 default:
-                    errorLabel.setText("Erro ao identificar o nível de acesso.");
+                    messageLabel.setText("Erro ao identificar o nível de acesso.");
             }
-
         } else {
-            System.out.println("Login falhou para o usuário: " + username);
-            errorLabel.setText("Usuário ou senha incorretos.");
+            messageLabel.setText("Usuário ou senha incorretos.");
         }
     }
 }
